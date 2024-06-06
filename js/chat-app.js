fcbc3948d7e898f1142e7f2b10d6d88788b9c265
hamataki <hamatakiryunosuke@gmail.com>
2024-06-06 16:01:40 +0900

    画像ファイル追加

---
+++ js/chat-app.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+  $("#imgUpload").on("change", function (e) {
+    $(this)
+      .next()
+      .text(
+        e.target.files.length ? e.target.files[0].name : "今はどんな表情？？"
+      );
+  });
+});
