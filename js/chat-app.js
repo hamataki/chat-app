f51ed13f4150d6d4e8db4fd780af8fa6184fb57e
hamataki <hamatakiryunosuke@gmail.com>
2024-06-07 08:04:23 +0900

    ファイルアップロードcss

--- js/chat-app.js
+++ js/chat-app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   $("#imgUpload").on("change", function (e) {
     $(this)
@@ -7,4 +6,15 @@ $(document).ready(function () {
         e.target.files.length ? e.target.files[0].name : "今はどんな表情？？"
       );
   });
+});
+$(document).on("change", "#imgUpload", function () {
+  let elem = this; //操作された要素を取得
+  let fileReader = new FileReader(); //ファイルを読み取るオブジェクトを生成
+  fileReader.readAsDataURL(elem.files[0]); //ファイルを読み取る
+  fileReader.onload = function () {
+    //ファイル読み取りが完了したら
+    let imgTag = `<img src='${fileReader.result}'>`; //img要素を生成
+    $("#preview").html(imgTag); //画像をプレビュー
+    
+  };
 });
